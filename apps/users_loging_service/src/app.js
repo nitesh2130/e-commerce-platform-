a5190d097cee44e8010e0d329784368d08d477e0
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-08-24 23:14:57 +0530

    This is setup getting the data json, urlencoded, static

--- apps/users_loging_service/src/app.js
+++ apps/users_loging_service/src/app.js
@@ -2,5 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credential: true ,
+        // whitelisting  please explain in details
+    })
+)
+
+app.use(express.json({limit: "5kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
 
 export default app
