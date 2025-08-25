29252865c83e4b9bf9219e0ded98811824539713
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-08-25 22:01:32 +0530

    This is about cookie parser

--- apps/users_loging_service/src/app.js
+++ apps/users_loging_service/src/app.js
@@ -13,6 +13,7 @@ app.use(
 app.use(express.json({limit: "5kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())  // to this we can setup only server is read and delete the cookie-parser 
 
 
 export default app
