b9751f82bec3906502e75f4707352d2d29208d42
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-09-02 12:36:42 +0530

    this is another way to handle the request

--- apps/users_loging_service/src/utils/asyncHandler.js
+++ apps/users_loging_service/src/utils/asyncHandler.js
@@ -1,10 +1,26 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            sucess: false,
-            message: err.message
-        })
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+
     }
 }
+
+
+
+
+// this method is handle request with another syntex
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             sucess: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+export {asyncHandler}
