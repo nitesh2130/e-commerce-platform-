1c6de83f118a2201f13ee257fa3c21a725470291
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-09-02 12:23:06 +0530

    This is setup the asyncHandler function the to handle request

---
+++ apps/users_loging_service/src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            sucess: false,
+            message: err.message
+        })
+    }
+}
