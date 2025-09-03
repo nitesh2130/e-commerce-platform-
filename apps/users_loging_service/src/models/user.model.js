4574e30af3209152e1d80723823c5669c88b67be
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-09-03 14:40:12 +0530

    this is schema of the user those are will be store the user data in the Database.

---
+++ apps/users_loging_service/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose'
+
+const userSchema = new Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model('User', userSchema)
