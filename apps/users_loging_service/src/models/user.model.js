274f362c378e360d5454723fba5230488d5137e4
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-09-03 18:47:13 +0530

    This is utilities function for the user service Hashed Password, Comapre password, Generate Access Token, Generate Refresh Token

--- apps/users_loging_service/src/models/user.model.js
+++ apps/users_loging_service/src/models/user.model.js
@@ -38,4 +38,45 @@ const userSchema = new Schema(
     }
 )
 
+//this is for hashed to the password
+userSchema.pre("save", async function(next) {
+    //if password is not modified, it pass to the next
+    if(!this.isModified(this.password)) return next();
+
+    // if password is modified, it do hash
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+
+// this is compare the password, password is correct or not
+userSchema.method.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// This is generate Access Token
+userSchema.method.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// This is generate Refresh Token
+userSchema.method.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model('User', userSchema)
