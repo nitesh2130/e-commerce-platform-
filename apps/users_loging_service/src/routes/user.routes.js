16a27777a1914bb2cba0ccaac29e99984a01b23c
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-09-12 13:38:44 +0530

    This is add middleware for the logout route

--- apps/users_loging_service/src/routes/user.routes.js
+++ apps/users_loging_service/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userRegister, userLogin, userLogOut } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.midleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -15,7 +16,7 @@ router.route("/register").post(
  )
 
 router.route("/login").post(userLogin);
-router.route("/logOut").delete(userLogOut);
+router.route("/logOut").delete(verifyJWT, userLogOut);
 
 export default router;
 
--- apps/users_loging_service/src/utils/cloudinary.js
+++ apps/users_loging_service/src/utils/cloudinary.js
@@ -28,7 +28,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     catch (error) {
         console.log(error, " this is error ")
         console.log("ab hum catch me ha ")
-        fs.unlinkSync(localFilePath) //remove the locally if file is not upload on the cloudinary. 
+        // const checkvar = fs.unlinkSync(localFilePath) //remove the locally if file is not upload on the cloudinary. 
+        console.log(checkvar, "  *      THIS IS RESULT FILE DELETE OR NOT");
         return null;
     }
 }
