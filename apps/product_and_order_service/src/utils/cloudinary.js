7aa2c56cd348dc5c44cf0b2a90d56a64c8c4bfa1
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-09-24 23:05:16 +0530

    This is some utilities function for the maker work easer

---
+++ apps/product_and_order_service/src/utils/cloudinary.js
@@ -0,0 +1,44 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_CLOUD_APIKEY, 
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET, 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    console.log(localFilePath, "this is localfile path in the cloudinary1")
+    try {
+        if(!localFilePath) return null;
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("response ", response);
+        // console.log("response ", response);
+        //files have been uploaded successfully
+        // console.log("file is uploaded on cloudinary, this is file url", response.url);
+
+        // First do this operation after that perfoam another task
+        // Unlink the file from the locally if file is uploaded successfully.
+        
+         fs.unlinkSync(localFilePath)
+       return { 
+        url: response.url, 
+        // publicId: response.public_id 
+};
+
+    }
+    catch (error) {
+        console.log(error, " this is error ")
+        console.log("ab hum catch me ha ")
+        // const checkvar = fs.unlinkSync(localFilePath) //remove the locally if file is not upload on the cloudinary. 
+        console.log(checkvar, "  *      THIS IS RESULT FILE DELETE OR NOT");
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary}
